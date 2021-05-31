6d9ba2057047004eb69cacb988712873ea1dc4be
Thomas <thomaspesler@gmail.com>
2021-05-31 15:09:08 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,8 +12,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.4",
-	name: "Uninflatey",
+	num: "0.5",
+	name: "So much content",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
